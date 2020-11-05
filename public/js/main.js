5d0995d8c58a9c76ae7834b9ccfb52bdfe0ca296
keshav sethi <keshav.sethi0004@gmail.com>
2020-11-05 15:52:03 +0530

    tables added

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-
-// REPLACE <...> BY YOUR FIREBASE PROJECT CONFIGURATION:
+var table = $('#table1').DataTable ( {
+    "bFilter": false
+} );
 const config = {
     apiKey: "AIzaSyCqOdIjUsNL95Uc00JBmhUWgHXtWCtNTLU",
     authDomain: "health-care-iot-534c9.firebaseapp.com",
@@ -11,7 +12,13 @@ const config = {
     measurementId: "G-3ZLX17QWM7"
   };
   firebase.initializeApp(config);
-  
+
+  firebase.database().ref('data/test').on('child_added',function(snapshot) {
+    var dataSet = [1, snapshot.val()];
+    table.rows.add([dataSet]).draw();
+    });
+    
+    
   console.log("check");
   firebase.database().ref('data/test').on('value', ts_measures => {
     let values = [];
